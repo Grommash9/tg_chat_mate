f8c3ab1dfea9e4977b705ec19f5c6287589d9bbb
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 19:27:13 +0000

    install file fix

--- install.sh
+++ install.sh
@@ -10,16 +10,17 @@ source .env
 set +a
 
 
-if [[ test5.telegram-crm.work =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ $SERVER_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "ip conf from sh skipped"
 else
     docker exec anonymous_support_bot-nginx-service-1 /bin/bash -c "apt-get update && apt-get install -y python3 && \
     apt-get install -y certbot python3-certbot-nginx && \
-    sed -i \"s/SERVER_IP_PLACEHOLDER/test5.telegram-crm.work/g\" /home/cert_bot_base.conf && \
-    sed -i \"s/SERVER_IP_PLACEHOLDER/test5.telegram-crm.work/g\" /home/cert_bot_default.conf && \
+    sed -i \"s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g\" /home/cert_bot_base.conf && \
+    sed -i \"s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g\" /home/cert_bot_default.conf && \
     cp /home/cert_bot_base.conf /etc/nginx/conf.d/default.conf && \
-    certbot --nginx -d test5.telegram-crm.work --register-unsafely-without-email --agree-tos --no-eff-email --force-renewal && \
-    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf "
+    certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email --force-renewal && \
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf && \
+    nginx -s reload "
 
 fi
 
