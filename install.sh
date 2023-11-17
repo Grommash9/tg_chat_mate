84c13ef07ca9870154f361b137828f6a887fb25d
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 18:35:52 +0000

    update

--- install.sh
+++ install.sh
@@ -15,13 +15,15 @@ if [[ $SERVER_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 else
     docker exec anonymous_support_bot-nginx-service-1 /bin/bash -c "apt-get update && apt-get install -y python3 && \
     apt-get install -y certbot python3-certbot-nginx && \
-    certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email && \
-    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/fullchain.pem /nginx-certs/$SERVER_IP_ADDRESS/fullchain.pem && \
-    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/privkey.pem /nginx-certs/$SERVER_IP_ADDRESS/privkey.pem && \
+    sed -i \"s/SERVER_IP_PLACEHOLDER/test2.telegram-crm.work/g\" /home/cert_bot_default.conf && \
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf && \
+    certbot --nginx -d test2.telegram-crm.work --register-unsafely-without-email --agree-tos --no-eff-email --force-renewal && \
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf && \
+    cp /etc/letsencrypt/live/test2.telegram-crm.work/fullchain.pem /nginx-certs/test2.telegram-crm.work/fullchain.pem && \
+    cp /etc/letsencrypt/live/test2.telegram-crm.work/privkey.pem /nginx-certs/test2.telegram-crm.work/privkey.pem && \
     cp /etc/letsencrypt/options-ssl-nginx.conf /nginx-certs/ && \
-    cp /etc/letsencrypt/ssl-dhparams.pem /nginx-certs/ && \
-    sed -i \"s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g\" /home/cert_bot_default.conf && \
-    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf"
+    cp /etc/letsencrypt/ssl-dhparams.pem /nginx-certs/ "
+
 fi
 
 docker compose down
