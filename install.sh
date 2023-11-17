7cc43a6e291eec43da55982e6b6f28258cd3ac76
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 19:13:01 +0000

    conf revoke

--- install.sh
+++ install.sh
@@ -19,11 +19,7 @@ else
     sed -i \"s/SERVER_IP_PLACEHOLDER/test5.telegram-crm.work/g\" /home/cert_bot_default.conf && \
     cp /home/cert_bot_base.conf /etc/nginx/conf.d/default.conf && \
     certbot --nginx -d test5.telegram-crm.work --register-unsafely-without-email --agree-tos --no-eff-email --force-renewal && \
-    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf && \
-    cp /etc/letsencrypt/live/test5.telegram-crm.work/fullchain.pem /nginx-certs/test5.telegram-crm.work/fullchain.pem && \
-    cp /etc/letsencrypt/live/test5.telegram-crm.work/privkey.pem /nginx-certs/test5.telegram-crm.work/privkey.pem && \
-    cp /etc/letsencrypt/options-ssl-nginx.conf /nginx-certs/ && \
-    cp /etc/letsencrypt/ssl-dhparams.pem /nginx-certs/ "
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf "
 
 fi
 
