6783cf95b96d9ca6ea3850d3b95db46df9fb7582
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 10:58:23 +0000

    domains logic added?

--- nginx-service/setup.sh
+++ nginx-service/setup.sh
@@ -9,18 +9,30 @@ apt-get update && apt-get install -y openssl
 # Create the necessary directories
 mkdir -p /etc/nginx/snippets /etc/ssl/private /nginx-certs
 
-# Generate self-signed SSL certificate and other configurations
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=New York/L=New York/O=Bouncy Castles, Inc./OU=Ministry of Water/CN=${SERVER_IP_ADDRESS}/emailAddress=admin@your_domain.com"
-openssl dhparam -out /etc/nginx/dhparam.pem 2048
 
-# Replace placeholder with actual server IP
-sed -i "s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g" /home/default.conf
 
-# Move configuration files into place
-cp /home/self-signed.conf /etc/nginx/snippets/
-cp /home/ssl-params.conf /etc/nginx/snippets/
-cp /home/default.conf /etc/nginx/conf.d/my-custom-server.conf
 
-# Copy the certificates to a custom directory
-cp /etc/ssl/certs/nginx-selfsigned.crt /nginx-certs/
-cp /etc/ssl/private/nginx-selfsigned.key /nginx-certs/
+if [[ $SERVER_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=New York/L=New York/O=Bouncy Castles, Inc./OU=Ministry of Water/CN=${SERVER_IP_ADDRESS}/emailAddress=admin@your_domain.com"
+    openssl dhparam -out /etc/nginx/dhparam.pem 2048
+
+    # Replace placeholder with actual server IP
+    sed -i "s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g" /home/default.conf
+
+    # Move configuration files into place
+    cp /home/self-signed.conf /etc/nginx/snippets/
+    cp /home/ssl-params.conf /etc/nginx/snippets/
+    cp /home/default.conf /etc/nginx/conf.d/my-custom-server.conf
+
+    # Copy the certificates to a custom directory
+    cp /etc/ssl/certs/nginx-selfsigned.crt /nginx-certs/
+    cp /etc/ssl/private/nginx-selfsigned.key /nginx-certs/
+else
+    # Domain name logic
+    if [ "$CLOUD_FLARE" = false ]; then
+        apt-get install -y certbot python3-certbot-nginx
+        certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email --nginx
+    fi
+    sed -i "s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g" /home/cert_bot_default.conf
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf
+fi
