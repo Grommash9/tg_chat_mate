030c028fb45e8711cec1c9cf948dc2c78b85f07a
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 11:24:12 +0000

    file fix

--- nginx-service/setup.sh
+++ nginx-service/setup.sh
@@ -33,6 +33,11 @@ else
         apt-get install -y certbot python3-certbot-nginx
         certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email --nginx
     fi
+    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/fullchain.pem /nginx-certs/$SERVER_IP_ADDRESS/
+    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/privkey.pem /nginx-certs/$SERVER_IP_ADDRESS/
+    cp /etc/letsencrypt/options-ssl-nginx.conf /nginx-certs/
+    cp /etc/letsencrypt/ssl-dhparams.pem /nginx-certs/
+    cp /etc/ssl/certs/nginx-selfsigned.crt /nginx-certs/
     sed -i "s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g" /home/cert_bot_default.conf
     cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf
 fi
