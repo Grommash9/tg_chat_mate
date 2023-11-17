85d105de9cc7dc327ba19441fa8a57f7042e6c58
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 18:05:29 +0000

    Revert "nginx-certbot added"
    This reverts commit df49b26eff8522a7ce3608ab60456d7d0d39e9e1.

--- nginx-service/setup.sh
+++ nginx-service/setup.sh
@@ -11,6 +11,7 @@ mkdir -p /etc/nginx/snippets /etc/ssl/private /nginx-certs
 mkdir -p /nginx-certs/$SERVER_IP_ADDRESS
 
 
+
 if [[ $SERVER_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=New York/L=New York/O=Bouncy Castles, Inc./OU=Ministry of Water/CN=${SERVER_IP_ADDRESS}/emailAddress=admin@your_domain.com"
     openssl dhparam -out /etc/nginx/dhparam.pem 2048
@@ -27,5 +28,13 @@ if [[ $SERVER_IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     cp /etc/ssl/certs/nginx-selfsigned.crt /nginx-certs/
     cp /etc/ssl/private/nginx-selfsigned.key /nginx-certs/
 else
-    
+    apt-get update && apt-get install -y python3 
+    apt-get install -y certbot python3-certbot-nginx
+    certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email
+    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/fullchain.pem /nginx-certs/$SERVER_IP_ADDRESS/fullchain.pem
+    cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/privkey.pem /nginx-certs/$SERVER_IP_ADDRESS/privkey.pem
+    cp /etc/letsencrypt/options-ssl-nginx.conf /nginx-certs/
+    cp /etc/letsencrypt/ssl-dhparams.pem /nginx-certs/
+    sed -i "s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g" /home/cert_bot_default.conf
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf
 fi
