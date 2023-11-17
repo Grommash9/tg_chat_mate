108ce11d13e02c23a9c077ad6bd8dca34a5c59b8
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-17 18:54:13 +0000

    conf to defult

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ else
     docker exec anonymous_support_bot-nginx-service-1 /bin/bash -c "apt-get update && apt-get install -y python3 && \
     apt-get install -y certbot python3-certbot-nginx && \
     sed -i \"s/SERVER_IP_PLACEHOLDER/$SERVER_IP_ADDRESS/g\" /home/cert_bot_default.conf && \
-    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf && \
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf && \
     certbot --nginx -d $SERVER_IP_ADDRESS --register-unsafely-without-email --agree-tos --no-eff-email --force-renewal && \
-    cp /home/cert_bot_default.conf /etc/nginx/conf.d/my-custom-server.conf && \
+    cp /home/cert_bot_default.conf /etc/nginx/conf.d/default.conf && \
     cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/fullchain.pem /nginx-certs/$SERVER_IP_ADDRESS/fullchain.pem && \
     cp /etc/letsencrypt/live/$SERVER_IP_ADDRESS/privkey.pem /nginx-certs/$SERVER_IP_ADDRESS/privkey.pem && \
     cp /etc/letsencrypt/options-ssl-nginx.conf /nginx-certs/ && \
