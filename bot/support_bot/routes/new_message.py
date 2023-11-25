172da1b5d614b52c1160f91f9c0dcaea7c5d80c1
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-25 14:12:52 +0000

    data types to send messages from manager added

--- bot/support_bot/routes/new_message.py
+++ bot/support_bot/routes/new_message.py
@@ -23,11 +23,14 @@ async def new_message_from_manager(request: Request):
     try:
         if file_attachment_id is not None:
             file_attachment = db.files.get_file(file_attachment_id)
-            if file_attachment["content_type"] == "application/pdf":
+            if file_attachment["content_type"].startswith("video/"):
+                message = await bot.send_video(chat_id, caption=message_text, video=BufferedInputFile(file_attachment["binary_data"], file_attachment["filename"]))
+            if file_attachment["content_type"].startswith("audio/"):
+                message = await bot.send_audio(chat_id, caption=message_text, audio=BufferedInputFile(file_attachment["binary_data"], file_attachment["filename"]))
+            if file_attachment["content_type"].startswith("image/"):
+                message = await bot.send_photo(chat_id, caption=message_text, photo=BufferedInputFile(file_attachment["binary_data"], file_attachment["filename"]))
+            if file_attachment["content_type"].startswith("application/"):
                 message = await bot.send_document(chat_id, caption=message_text, document=BufferedInputFile(file_attachment["binary_data"], file_attachment["filename"]))
-                
-            # message = await bot.send_message(chat_id, message)
-            # message = await bot.send_message(chat_id, message)
             message_document = db.message.new_message(message, unread=False, attachment=file_attachment)
         else:
             message = await bot.send_message(chat_id, message_text)
