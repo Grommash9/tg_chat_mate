ea1f98ac7fdb971fb5a84b6a81f23d551f135ec8
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-11-28 23:37:40 +0000

    DEBIAN_FRONTEND

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker --version
