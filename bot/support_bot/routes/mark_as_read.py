0aa0395dcdb8863c5668bf3e11d961304eb2f2eb
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-12-02 10:51:31 +0000

    some new design fixes

--- bot/support_bot/routes/mark_as_read.py
+++ bot/support_bot/routes/mark_as_read.py
@@ -27,25 +27,25 @@ async def mark_message_as_read(request: Request):
     return set_cors_headers(response)
 
 
-@web_routes.post(f"/tg-bot/mark-chat-as-read")
-async def mark_chat_as_read(request: Request):
-    token = request.cookies.get("AUTHToken")
-    if not token:
-        token = request.headers.get("AuthorizationToken")
-    manager = db.manager.get_manager_by_token(token)
-    if manager is None:
-        response = web.json_response(
-            {"result": "AuthorizationToken", "modified_count": 0}, status=401
-        )
-        return set_cors_headers(response)
+# @web_routes.post(f"/tg-bot/mark-chat-as-read")
+# async def mark_chat_as_read(request: Request):
+#     token = request.cookies.get("AUTHToken")
+#     if not token:
+#         token = request.headers.get("AuthorizationToken")
+#     manager = db.manager.get_manager_by_token(token)
+#     if manager is None:
+#         response = web.json_response(
+#             {"result": "AuthorizationToken", "modified_count": 0}, status=401
+#         )
+#         return set_cors_headers(response)
 
-    payload = await request.json()
-    chat_id = payload.get("chat_id")
-    modified_count = db.message.mark_chat_as_read(chat_id)
-    response = web.json_response(
-        {"result": "Marked!", "modified_count": modified_count}, status=200
-    )
-    return set_cors_headers(response)
+#     payload = await request.json()
+#     chat_id = payload.get("chat_id")
+#     modified_count = db.message.mark_chat_as_read(chat_id)
+#     response = web.json_response(
+#         {"result": "Marked!", "modified_count": modified_count}, status=200
+#     )
+#     return set_cors_headers(response)
 
 
 @web_routes.options("/tg-bot/mark-as-read")
