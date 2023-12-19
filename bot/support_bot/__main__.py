f166ac628f45ee8622f49fec1a4824099c2d442d
Oleksandr Prudnikov <prudnikov21@icloud.com>
2023-12-19 19:20:06 +0000

    update

--- bot/support_bot/__main__.py
+++ bot/support_bot/__main__.py
@@ -1,7 +1,15 @@
 import logging
-import sys
+
+
+
+
+
 
 from support_bot.misc import start_bot
+import sys
+
+
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
