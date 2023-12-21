bd8288c41d82fb0bdc102a4e622210d84c5d2108
Oleksandr Prudnikov <prudnikov21@icloud.com>
2023-12-21 11:26:52 +0000

    new send message endpoints logic

--- bot/support_bot/routes/new_message.py
+++ bot/support_bot/routes/new_message.py
@@ -12,7 +12,7 @@ from support_bot.misc import (
 )
 
 
-@web_routes.post("/tg-bot/new-message")
+@web_routes.post("/tg-bot/new-text-message")
 async def new_message_from_manager(request: Request):
     manager_username = get_manager_from_request(request)
     if manager_username is None:
@@ -22,74 +22,178 @@ async def new_message_from_manager(request: Request):
         return set_cors_headers(response)
 
     manager = db.manager.get_manager_by_username(manager_username)
+    payload = await request.json()
+    chat_id = payload.get("chat_id")
+    message_text = payload.get("text")
+    try:
+        message = await bot.send_message(chat_id, message_text)
+        message_document = db.message.new_message(
+            message, unread=False, manager_name=manager["full_name"]
+        )
+        await send_update_to_socket(message_document)
+        response = web.json_response({"result": "Sent"}, status=200)
+    except Exception as e:
+        response = web.json_response({"result": str(e)}, status=500)
+    return set_cors_headers(response)
 
+
+@web_routes.post("/tg-bot/new-document-message")
+async def new_document_message_from_manager(request: Request):
+    manager_username = get_manager_from_request(request)
+    if manager_username is None:
+        response = web.json_response(
+            {"result": "AuthorizationToken"}, status=401
+        )
+        return set_cors_headers(response)
+
+    manager = db.manager.get_manager_by_username(manager_username)
     payload = await request.json()
     chat_id = payload.get("chat_id")
     message_text = payload.get("text")
     file_attachment_id = payload.get("file_attachment_id")
+    file_attachment = db.files.get_file(file_attachment_id)
     try:
-        if file_attachment_id is None:
-            message = await bot.send_message(chat_id, message_text)
-            message_document = db.message.new_message(
-                message, unread=False, manager_name=manager["full_name"]
+        message = await bot.send_document(
+                chat_id,
+                caption=message_text,
+                document=BufferedInputFile(
+                    file_attachment["binary_data"],
+                    file_attachment["filename"],
+                ),
             )
-        else:
-            file_attachment = db.files.get_file(file_attachment_id)
-            if file_attachment["content_type"].startswith("video/"):
-                message = await bot.send_video(
-                    chat_id,
-                    caption=message_text,
-                    video=BufferedInputFile(
-                        file_attachment["binary_data"],
-                        file_attachment["filename"],
-                    ),
-                )
-            elif file_attachment["content_type"].startswith("audio/"):
-                message = await bot.send_audio(
-                    chat_id,
-                    caption=message_text,
-                    audio=BufferedInputFile(
-                        file_attachment["binary_data"],
-                        file_attachment["filename"],
-                    ),
-                )
-            elif (
-                file_attachment["content_type"].startswith("application/")
-                or file_attachment["content_type"].startswith("text/")
-                or file_attachment["content_type"] == "image/svg+xml"
-            ):
-                message = await bot.send_document(
-                    chat_id,
-                    caption=message_text,
-                    document=BufferedInputFile(
-                        file_attachment["binary_data"],
-                        file_attachment["filename"],
-                    ),
-                )
-            elif file_attachment["content_type"].startswith("image/"):
-                message = await bot.send_photo(
-                    chat_id,
-                    caption=message_text,
-                    photo=BufferedInputFile(
-                        file_attachment["binary_data"],
-                        file_attachment["filename"],
-                    ),
-                )
-            else:
-                raise FileNotFoundError(
-                    f"Can't process {file_attachment['content_type']} "
-                    "file type with telegram, please contact administrator"
-                )
-            message_document = db.message.new_message(
-                message,
-                unread=False,
-                attachment={
-                    "file_id": file_attachment_id,
-                    "mime_type": file_attachment["content_type"],
-                    "file_name": file_attachment["filename"],
-                },
-                manager_name=manager["full_name"],
+        message_document = db.message.new_message(
+            message,
+            unread=False,
+            attachment={
+                "file_id": file_attachment_id,
+                "mime_type": file_attachment["content_type"],
+                "file_name": file_attachment["filename"],
+            },
+            manager_name=manager["full_name"],
+        )
+        await send_update_to_socket(message_document)
+        response = web.json_response({"result": "Sent"}, status=200)
+    except Exception as e:
+        response = web.json_response({"result": str(e)}, status=500)
+    return set_cors_headers(response)
+
+
+@web_routes.post("/tg-bot/new-photo-message")
+async def new_photo_message_from_manager(request: Request):
+    manager_username = get_manager_from_request(request)
+    if manager_username is None:
+        response = web.json_response(
+            {"result": "AuthorizationToken"}, status=401
+        )
+        return set_cors_headers(response)
+
+    manager = db.manager.get_manager_by_username(manager_username)
+    payload = await request.json()
+    chat_id = payload.get("chat_id")
+    message_text = payload.get("text")
+    file_attachment_id = payload.get("file_attachment_id")
+    file_attachment = db.files.get_file(file_attachment_id)
+    try:
+        message = await bot.send_photo(
+                chat_id,
+                caption=message_text,
+                photo=BufferedInputFile(
+                    file_attachment["binary_data"],
+                    file_attachment["filename"],
+                ),
             )
+        message_document = db.message.new_message(
+            message,
+            unread=False,
+            attachment={
+                "file_id": file_attachment_id,
+                "mime_type": file_attachment["content_type"],
+                "file_name": file_attachment["filename"],
+            },
+            manager_name=manager["full_name"],
+        )
+        await send_update_to_socket(message_document)
+        response = web.json_response({"result": "Sent"}, status=200)
+    except Exception as e:
+        response = web.json_response({"result": str(e)}, status=500)
+    return set_cors_headers(response)
+
+
+@web_routes.post("/tg-bot/new-audio-message")
+async def new_audio_message_from_manager(request: Request):
+    manager_username = get_manager_from_request(request)
+    if manager_username is None:
+        response = web.json_response(
+            {"result": "AuthorizationToken"}, status=401
+        )
+        return set_cors_headers(response)
+
+    manager = db.manager.get_manager_by_username(manager_username)
+    payload = await request.json()
+    chat_id = payload.get("chat_id")
+    message_text = payload.get("text")
+    file_attachment_id = payload.get("file_attachment_id")
+    file_attachment = db.files.get_file(file_attachment_id)
+    try:
+        message = await bot.send_audio(
+                chat_id,
+                caption=message_text,
+                audio=BufferedInputFile(
+                    file_attachment["binary_data"],
+                    file_attachment["filename"],
+                ),
+            )
+        message_document = db.message.new_message(
+            message,
+            unread=False,
+            attachment={
+                "file_id": file_attachment_id,
+                "mime_type": file_attachment["content_type"],
+                "file_name": file_attachment["filename"],
+            },
+            manager_name=manager["full_name"],
+        )
+        await send_update_to_socket(message_document)
+        response = web.json_response({"result": "Sent"}, status=200)
+    except Exception as e:
+        response = web.json_response({"result": str(e)}, status=500)
+    return set_cors_headers(response)
+
+
+@web_routes.post("/tg-bot/new-video-message")
+async def new_video_message_from_manager(request: Request):
+    manager_username = get_manager_from_request(request)
+    if manager_username is None:
+        response = web.json_response(
+            {"result": "AuthorizationToken"}, status=401
+        )
+        return set_cors_headers(response)
+
+    manager = db.manager.get_manager_by_username(manager_username)
+    payload = await request.json()
+    chat_id = payload.get("chat_id")
+    message_text = payload.get("text")
+    file_attachment_id = payload.get("file_attachment_id")
+    file_attachment = db.files.get_file(file_attachment_id)
+    try:
+        message = await bot.send_video(
+                chat_id,
+                caption=message_text,
+                video=BufferedInputFile(
+                    file_attachment["binary_data"],
+                    file_attachment["filename"],
+                ),
+            )
+        message_document = db.message.new_message(
+            message,
+            unread=False,
+            attachment={
+                "file_id": file_attachment_id,
+                "mime_type": file_attachment["content_type"],
+                "file_name": file_attachment["filename"],
+            },
+            manager_name=manager["full_name"],
+        )
         await send_update_to_socket(message_document)
         response = web.json_response({"result": "Sent"}, status=200)
     except Exception as e:
