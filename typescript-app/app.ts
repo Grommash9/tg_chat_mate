f1b313b862e87f24112750462764ba25bd2a787b
Oleksandr Prudnikov <prudnikov21@icloud.com>
2023-12-22 08:22:55 +0000

    fix static files

--- typescript-app/app.ts
+++ typescript-app/app.ts
@@ -14,54 +14,15 @@ const io = new SocketIOServer(httpServer);
 const cors = require('cors');
 const axiosInstance = axios.create({
   httpsAgent: new https.Agent({
-    rejectUnauthorized: false // Disable SSL certificate validation
+    rejectUnauthorized: false
   })
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.json()); // Middleware to parse JSON requests
+console.log('__dirname is: ', __dirname);
+
+app.use(express.static(__dirname));
+app.use(express.json());
 app.use(cors());
-
-// have no idea why it's not working as static file
-app.get('/main.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'main.css'));
-});
-
-app.get('/index.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.js'));
-});
-
-app.get('/login.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.js'));
-});
-
-app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'favicon.ico'));
-});
-
-app.get('/mixkit-correct-answer-tone-2870.wav', (req, res) => {
-  res.sendFile(path.join(__dirname, 'mixkit-correct-answer-tone-2870.wav'));
-});
-
-app.get('/user_empty_photo.png', (req, res) => {
-  res.sendFile(path.join(__dirname, 'user_empty_photo.png'));
-});
-
-app.get('/manager_empty_photo.png', (req, res) => {
-  res.sendFile(path.join(__dirname, 'manager_empty_photo.png'));
-});
-
-app.get('/mixkit-message-pop-alert-2354.mp3', (req, res) => {
-  res.sendFile(path.join(__dirname, 'mixkit-message-pop-alert-2354.mp3'));
-});
-
-app.get('/normal.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'normal.css'));
-});
-
-app.get('/login.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.css'));
-});
 
 app.get('/', (req, res) => {
   const token = req.cookies['AUTHToken'];
@@ -77,18 +38,14 @@ app.get('/', (req, res) => {
     })
     .then((response) => {
       console.log('Status:', response.status);
-      // Check if the status code is 200
       if (response.status !== 200) {
-        // If not, send the login page
         res.sendFile(path.join(__dirname, 'login.html'));
         throw new Error(`Network response was not ok: ${response.status}`);
       }
-      // If the status code is 200, send the index page
       res.sendFile(path.join(__dirname, 'index.html'));
       console.log('Success:', response.data);
     })
     .catch((error) => {
-      // Log the error and send the login page
       console.error('Error:', error);
       res.sendFile(path.join(__dirname, 'login.html'));
     });
@@ -98,7 +55,6 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-// Endpoint to send a message to the server and broadcast it to all connected clients via Socket.IO
 app.post('/send-message', (req, res) => {
   const xRealIP = req.headers['x-real-ip']!;
   const ip = Array.isArray(xRealIP) ? xRealIP[0] : xRealIP;
