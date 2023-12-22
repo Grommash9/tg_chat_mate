95d4e59f1a10a98cccadfaedfa561d70b51d90f2
Grommash9 <67575598+Grommash9@users.noreply.github.com>
2023-12-22 09:26:42 +0000

    prettier add to ts and build config added (#111)
    * prettier add to ts and build config added
    
    * login logic move into ts
    
    * fix static files
    
    * files move into folders, static files logic improve, chat setting panel fixed error

---
+++ typescript-app/frontend/login.ts
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let login_button = document.getElementById(
+    'login-button-object'
+  ) as HTMLButtonElement;
+  login_button.addEventListener('click', SignIn);
+
+  let email_input = document.getElementById(
+    'login-input-object'
+  ) as HTMLInputElement;
+  let password_input = document.getElementById(
+    'password-input-object'
+  ) as HTMLInputElement;
+
+  email_input.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+      SignIn();
+    }
+  });
+  password_input.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+      SignIn();
+    }
+  });
+});
+
+function SignIn() {
+  let email_input = document.getElementById(
+    'login-input-object'
+  ) as HTMLInputElement;
+  let password_input = document.getElementById(
+    'password-input-object'
+  ) as HTMLInputElement;
+  let payload = {
+    user_name: email_input.value,
+    password: password_input.value
+  };
+  fetch(`/tg-bot/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json().then((data) => {
+        password_input.value = '';
+        setCookie('AUTHToken', data['token'], 7);
+        console.log('Success:', data);
+        window.location.replace('/');
+      });
+    } else {
+      return response.json().then((data) => {
+        password_input.value = '';
+        alert(`Error: ${data['error']}`);
+        console.error('Error:', data['error']);
+      });
+    }
+  });
+
+  function setCookie(name: string, value: string, daysToLive: number) {
+    var cookieValue = encodeURIComponent(value);
+    var cookie = name + '=' + cookieValue;
+
+    if (typeof daysToLive === 'number') {
+      cookie += '; max-age=' + daysToLive * 24 * 60 * 60;
+      cookie += '; path=/';
+    }
+
+    document.cookie = cookie;
+  }
+}
