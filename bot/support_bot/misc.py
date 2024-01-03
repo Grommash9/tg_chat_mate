d0602286b9fb0116dd2e3cabdce5e08d859415a4
Alena-Fedotieva <alenafedotieva@gmail.com>
2024-01-03 23:46:52 +0200

    added description

--- bot/support_bot/misc.py
+++ bot/support_bot/misc.py
@@ -174,6 +174,9 @@ class ChatTypeFilter(BaseFilter):
 
 
 class BannedUserCheck(BaseMiddleware):
+    """
+    Middleware to ignore updates from users which are banned.
+    """
     async def user_banned(self, user: User) -> bool:
         user_info = await db.user.get_user(user.id)
         if not user_info:
