cd9138bd83ee1098c0e6bb59c2d39d264985caad
boris.nosatiuk@gmail.com <sJtL6HcWgL@QB.s>
2024-01-10 12:48:34 +0000

    +

--- ts-linting.sh
+++
@@ -1,5 +0,0 @@
-echo "Running TypeScript linting"
-cd typescript-app
-npm install
-npx prettier --check --ignore-path .prettierignore frontend app.ts
-echo "TypeScript linting completed successfully"
