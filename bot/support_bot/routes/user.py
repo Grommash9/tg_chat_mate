e7f4646eb58b32918023f9467f710530409013d9
PyGeek33 <112251977+PyGeek33@users.noreply.github.com>
2024-01-13 15:23:13 +0200

    add network to redis in docker_compose (#153)
    * add network to redis in docker_compose
    add aioredis to pipfile
    create endpoint manager activity notification
    and add test for this endpoint
    
    * done local pipelines
    
    * update logic mypy in pipeline
    fix user endpoint params unpacking
    fix check_action_flood_protection function
    add Global valuable ACTION_LIST
    
    ---------
    
    Co-authored-by: boris.nosatiuk@gmail.com <sJtL6HcWgL@QB.s>

--- bot/support_bot/routes/user.py
+++ bot/support_bot/routes/user.py
@@ -2,16 +2,20 @@ from aiohttp import web
 from aiohttp.web_request import Request
 
 import support_bot.db.user
-from support_bot.misc import web_routes
-from support_bot.routes.utils import require_auth
+from support_bot.misc import bot, web_routes
+from support_bot.routes.utils import (
+    check_action_flood_protection,
+    check_action_value,
+    require_auth,
+)
 
 
 @web_routes.get("/tg-bot/user/{user_id}")
 @require_auth
 async def get_user_info(request: Request):
     try:
-        user_id = int(request.match_info.get("user_id", 0))
-    except (ValueError, TypeError) as e:
+        user_id = int(request.match_info["user_id"])
+    except (ValueError, TypeError, KeyError) as e:
         return web.json_response(
             {"result": f"user_id param error: {e}"}, status=400
         )
@@ -26,8 +30,8 @@ async def get_user_info(request: Request):
 async def user_update(request: Request):
     data = await request.json()
     try:
-        user_id = int(request.match_info.get("user_id", 0))
-    except (ValueError, TypeError) as e:
+        user_id = int(request.match_info["user_id"])
+    except (ValueError, TypeError, KeyError) as e:
         return web.json_response(
             {"result": f"user_id param error: {e}"}, status=400
         )
@@ -35,3 +39,33 @@ async def user_update(request: Request):
     if update_user_info:
         return web.json_response({"result": "User info updated"}, status=200)
     return web.json_response({"result": "User not found"}, status=404)
+
+
+@web_routes.post("/tg-bot/user/{user_id}/{action}")
+@require_auth
+async def manager_action_notification(request: Request):
+    try:
+        user_id = int(request.match_info["user_id"])
+        action = check_action_value(request)
+    except (ValueError, TypeError, KeyError) as e:
+        return web.json_response(
+            {"result": f"user_id or action param error: {e}"}, status=400
+        )
+
+    flood_protection = await check_action_flood_protection(user_id)
+    if flood_protection:
+        return web.json_response(
+            {"result": "notification don`t pass flood check"}, status=429
+        )
+
+    try:
+        await bot.send_chat_action(chat_id=user_id, action=action)
+    except Exception as e:
+        return web.json_response(
+            {"result": f"cant send notification to user, error {e}"},
+            status=500,
+        )
+
+    return web.json_response(
+        {"result": "notification sent successfully"}, status=200
+    )
