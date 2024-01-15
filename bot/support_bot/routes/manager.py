8d6c73d18e85fdebf79ec30ec90f9ad95f2a6723
Oleksandr Prudnikov <prudnikov21@icloud.com>
2024-01-15 07:36:35 +0000

    review feedback fix

--- bot/support_bot/routes/manager.py
+++ bot/support_bot/routes/manager.py
@@ -12,7 +12,6 @@ from support_bot.db.manager import (
 from support_bot.misc import (
     create_token_for_manager,
     get_manager_username_from_jwt,
-    set_cors_headers,
     web_routes,
 )
 from support_bot.routes.utils import create_option_response, require_auth
@@ -21,28 +20,25 @@ from support_bot.routes.utils import create_option_response, require_auth
 @web_routes.post("/tg-bot/manager/login")
 async def manager_login(request: Request):
     payload = await request.json()
-    username = payload.get("username")
-    password = payload.get("password")
-    if not username or not password:
+    try:
+        username, password = payload["username"], payload["password"]
+    except KeyError:
         return json_response(
-            {"error": "Missing username or password"}, status=400
+            {"error": "Missing username or password"}, status=422
         )
 
     if not await db.manager.check_password(username, password):
-        response = json_response({"error": "Wrong credentials"}, status=401)
-        return set_cors_headers(response)
+        return json_response({"error": "Wrong credentials"}, status=401)
     manager = await get_manager_by_username(username)
 
     if manager is not None and manager.activated:
         token = create_token_for_manager(username)
-        response = json_response({"token": token}, status=200)
-        return set_cors_headers(response)
+        return json_response({"token": token}, status=200)
 
     error_text = (
         "Your account is not active yet. Please contact administrator."
     )
-    response = json_response({"error": error_text}, status=401)
-    return set_cors_headers(response)
+    return json_response({"error": error_text}, status=401)
 
 
 @web_routes.post("/tg-bot/manager/check_token")
@@ -51,14 +47,12 @@ async def manager_check_token(request: Request):
     token = payload.get("token")
     manager_user_name = get_manager_username_from_jwt(token)
     if manager_user_name is None:
-        response = json_response(
+        return json_response(
             {"error": "Wrong token or it was expired"}, status=401
         )
-    else:
-        response = json_response(
-            {"token": token, "manager": manager_user_name}, status=200
-        )
-    return set_cors_headers(response)
+    return json_response(
+        {"token": token, "manager": manager_user_name}, status=200
+    )
 
 
 @web_routes.post("/tg-bot/manager/change-password")
@@ -70,19 +64,16 @@ async def manager_change_password(request: Request):
         new_password = payload["new_password"]
         old_password = payload["old_password"]
     except KeyError:
-        response = json_response(
+        return json_response(
             {"result": "new_password or old_password fields missing!"},
             status=422,
         )
-        return set_cors_headers(response)
     if not await check_password(user_name, old_password):
-        response = json_response({"result": "Wrong password!"}, status=401)
-        return set_cors_headers(response)
+        return json_response({"result": "Wrong password!"}, status=401)
 
     hashed_password = hash_password(new_password)
     await update(user_name, {"hashed_password": hashed_password})
-    response = json_response({"result": "Password changed!"}, status=201)
-    return set_cors_headers(response)
+    return json_response({"result": "Password changed!"}, status=201)
 
 
 @web_routes.get("/tg-bot/manager/get-me")
@@ -90,61 +81,58 @@ async def manager_change_password(request: Request):
 async def get_manager_info(request: Request):
     manager = await get_manager_by_username(request["manager_user_name"])
     if manager is None:
-        response = json_response({"manager_info": {}}, status=404)
-    else:
-        response = json_response(
-            {"manager_info": manager.to_dict()}, status=200
-        )
-    return set_cors_headers(response)
+        return json_response({"manager_info": {}}, status=404)
+    return json_response({"manager_info": manager.to_dict()}, status=200)
 
 
 @web_routes.get("/tg-bot/manager")
 @require_auth
 async def get_managers(request: Request):
     managers = await db.manager.get_managers()
-    response = json_response(
-        {"managers": [manager.to_dict() for manager in managers]}, status=200
-    )
-    return set_cors_headers(response)
+    manager_list = [manager.to_dict() for manager in managers]
+    return json_response({"managers": manager_list}, status=200)
 
 
 @web_routes.patch("/tg-bot/manager")
 @require_auth
 async def update_manager(request: Request):
     payload = await request.json()
-    username = payload.get("username")
+    try:
+        username = payload["username"]
+    except KeyError:
+        return json_response(
+            {"result": "Manager username is missing"}, status=422
+        )
     update_counter = await update(username, payload)
     if update_counter:
-        response = json_response(
-            {"result": "Manager info updated"}, status=200
-        )
-    else:
-        response = json_response(
-            {"result": "Manager info was not updated"}, status=500
-        )
-    return set_cors_headers(response)
+        return json_response({"result": "Manager info updated"}, status=200)
+    return json_response(
+        {"result": "Manager info was not updated"}, status=500
+    )
 
 
 @web_routes.delete("/tg-bot/manager")
 @require_auth
 async def delete_manager(request: Request):
     payload = await request.json()
-    username = payload.get("username")
-    if not username:
-        response = json_response({"error": "Missing username"}, status=400)
-    else:
-        await db.manager.delete_manager_by_username(username)
-        response = json_response({}, status=204)
-        return set_cors_headers(response)
+    try:
+        username = payload["username"]
+    except KeyError:
+        return json_response(
+            {"result": "Manager username is missing"}, status=422
+        )
+    await db.manager.delete_manager_by_username(username)
+    return json_response({}, status=204)
 
 
 @web_routes.post("/tg-bot/manager")
 async def manager_registration(request: Request):
     payload = await request.json()
-    username = payload.get("username")
-    password = payload.get("password")
-    full_name = payload.get("full_name")
-    if not username or not password or not full_name:
+    try:
+        username = payload["username"]
+        password = payload["password"]
+        full_name = payload["full_name"]
+    except KeyError:
         return json_response(
             {"error": "Missing username or password or full name"}, status=400
         )
@@ -158,12 +146,8 @@ async def manager_registration(request: Request):
             full_name, username, password, activated=False
         )
     except DuplicateKeyError as e:
-        response = json_response({"result": str(e)}, status=409)
-    else:
-        response = json_response(
-            {"result": "ok", "username": username}, status=201
-        )
-    return set_cors_headers(response)
+        return json_response({"result": str(e)}, status=409)
+    return json_response({"result": "ok", "username": username}, status=201)
 
 
 @web_routes.options("/tg-bot/<action>")
