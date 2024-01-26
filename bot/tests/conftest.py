9e73a86a13cc2f2cf4e8864d0597afb52bcaa874
Jonatan Chaverri <jonathan.chaverri12@gmail.com>
2024-01-26 14:28:53 -0600

    Fix test_client fixture (#183)
    Co-authored-by: Jonatan Chaverri <jonatan.chaverri@kyndryl.com>

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -1,12 +1,16 @@
 import pytest
+from aiohttp import web
 
-from support_bot.misc import app, web_routes
+from support_bot.misc import web_routes
 
 
 @pytest.fixture
-def test_client(loop, aiohttp_client):
+def test_client(event_loop, aiohttp_client):
     """
     Test client fixture. Use it to make requests to the server.
     """
-    app.add_routes(web_routes)
-    return loop.run_until_complete(aiohttp_client(app))
+    app = web.Application()
+    if not app.get("routes_added", False):
+        app.add_routes(web_routes)
+        app["routes_added"] = True
+    return event_loop.run_until_complete(aiohttp_client(app))
