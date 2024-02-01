ccf718edbc698d313c744b4528678057ee25487c
boris <boris.nosatiuk@gmail.com>
2024-02-01 12:41:54 +0000

    ran flake

--- bot/tests/post_deploy_tests.py
+++ bot/tests/post_deploy_tests.py
@@ -4,7 +4,9 @@ from os import getenv
 import pytest
 import requests
 
-DOMAIN = getenv("DOMAIN", "386b-2a0a-ef40-863-3801-791a-9e86-45b0-37be.ngrok-free.app")
+DOMAIN = getenv(
+    "DOMAIN", "3801-791a-9e86-45b0-37be.ngrok-free.app"
+)
 USER_NAME = "root"
 PASSWORD = getenv("ROOT_PASSWORD", "root_strong_password")
 
